8448e966662ae536178a78a72d86006bfe67673d
tiagosardi <tiagosardi@gmail.com>
2021-08-31 13:08:58 -0300

    criando banco de dados e middlewares iniciais

--- src/app.ts
+++ src/app.ts
@@ -8,13 +8,30 @@ export class App{
     constructor(){
         this.express = express();
         this.listen();
+        this.middlewares();
+        this.database();
     }
     public getApp(): express.Application{
         return this.express;
     }
+
+    private middlewares(): void{
+        this.express.use(express.json());
+        this.express.use(cors());
+    }
+
+    
+
     private listen(){
         this.express.listen(this.porta, () =>{
             console.log('Servidor iniciado na porta ' + this.porta);
+        });
+    }
+
+    private database(): void{
+        mongoose.connect('mongodb+srv://tiagosardi:cachaça31@cluster0.qb4px.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
         });
     }
 
