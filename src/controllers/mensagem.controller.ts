5d63e72b9f9fb7268b3b5efca703fb7ba3a7139f
tiagosardi <tiagosardi@gmail.com>
2021-08-31 22:40:33 -0300

    configurando JWT e gerando token de usuário

--- src/controllers/mensagem.controller.ts
+++ src/controllers/mensagem.controller.ts
@@ -3,13 +3,11 @@ import mensagemModel from "../models/mensagem.model";
 
 class MensagemController{
     public async enviar(req: Request, res: Response): Promise<Response>{
-        const mensagem = await mensagemModel.create(
-            {
-                texto: req.body.texto,
-                remetente: '',
-                destinatario:''
-            }
-        );
+        const mensagem = await mensagemModel.create({
+            texto: req.body.texto,
+            remetente: '',
+            destinatario:req.params.id
+        });
         return res.json(mensagem);
     }
 }
--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-
+import jwt from 'jsonwebtoken';
+import usuarioModel from "../models/usuario.model";
 class AuthMiddleware{
     public autorizarUsuarioByToken(req: Request, res: Response){
         const token = req.query.token || req.headers['x-acess-token'];
@@ -7,6 +8,9 @@ class AuthMiddleware{
             return res.status(401).send({message: 'Acesso Negado!'})
             //401 eh um erro de autorizacao
         }
+
+        const usuarioToken = jwt.verify(token, 'SECRET')
+        const usuario = await usuarioModel.findById(usuarioToken._id);
         return next();
     }
 }
